a99e9bce8eee3ffa7fae48495f91e7aa3d398e27
immp-32 <96933713+immp-32@users.noreply.github.com>
2022-04-17 10:34:02 +0545

    Update BMI_Calculate.js

--- Classes/CodingChallenge/BMI_Calculate.js
+++ Classes/CodingChallenge/BMI_Calculate.js
@@ -1,11 +1,11 @@
-prabin_mass = 80;
-prabin_height = 1.6764;
+let prabin_mass = 80;
+let prabin_height = 1.6764;
 
-ram_mass = 60;
-ram_height = 1.76784;
+let ram_mass = 60;
+let ram_height = 1.76784;
 
-BMI1 = prabin_mass / (prabin_height * prabin_height);
-BMI2 = ram_mass / (ram_height * ram_height);
+let BMI1 = prabin_mass / (prabin_height * prabin_height);
+let BMI2 = ram_mass / (ram_height * ram_height);
 
 // console.log(BMI1);
 // console.log(BMI2);
